40fbfaf7c1e19251fd049735dcc2dbacc122ee19
pablobidiaki <pablobidiaki@hotmail.com>
2022-12-29 17:15:43 -0300

    ajustes

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 const mais = document.getElementById('mais')
 const menos = document.getElementById('menos')
 let quant = document.getElementById('resultado')
-quantidade = 0
+let quantidade = 0
+
+const esq = document.getElementById('esquerda')
+const dir = document.getElementById('direita')
+const img1 = 'url(imgs/image-product-1.jpg)'
+const img2 = 'url(imgs/image-product-2.jpg)'
+const img3 = 'url(imgs/image-product-3.jpg)'
+const img4 = 'url(imgs/image-product-4.jpg)'
+let contagemImg = 1 
+
+const botaoBurguer = document.getElementById('burguer')
+const menuBurguer = document.getElementById('menuBurguer')
+const main = document.getElementById('main')
+const header = document.getElementById('header')
+const botaoFechar = document.getElementById('btnFechar')
 
 mais.addEventListener('click', () =>{
     quantidade += 1
@@ -15,4 +29,52 @@ menos.addEventListener('click', () =>{
         quantidade -= 1
         quant.innerHTML = quantidade
     }
+})
+
+esq.addEventListener('click', () =>{
+    contagemImg -= 1
+    if(contagemImg < 1){contagemImg = 4}
+
+    if(contagemImg == 1){
+        document.getElementById('imagemPrincipal').style.backgroundImage = img1
+    }
+    if(contagemImg == 2){
+        document.getElementById('imagemPrincipal').style.backgroundImage = img2
+    }
+    if(contagemImg == 3){
+        document.getElementById('imagemPrincipal').style.backgroundImage = img3
+    }
+    if(contagemImg == 4){
+        document.getElementById('imagemPrincipal').style.backgroundImage = img4
+    }
+})
+
+dir.addEventListener('click', () =>{
+    contagemImg += 1
+    if(contagemImg > 4){contagemImg = 1}
+
+    if(contagemImg == 1){
+        document.getElementById('imagemPrincipal').style.backgroundImage = img1
+    }
+    if(contagemImg == 2){
+        document.getElementById('imagemPrincipal').style.backgroundImage = img2
+    }
+    if(contagemImg == 3){
+        document.getElementById('imagemPrincipal').style.backgroundImage = img3
+    }
+    if(contagemImg == 4){
+        document.getElementById('imagemPrincipal').style.backgroundImage = img4
+    }
+})
+
+botaoBurguer.addEventListener('click', () =>{
+    menuBurguer.classList.remove('none')
+    main.classList.add('none')
+    header.classList.add('none')
+})
+
+botaoFechar.addEventListener('click', () =>{
+    menuBurguer.classList.add('none')
+    main.classList.remove('none')
+    header.classList.remove('none')
 })
